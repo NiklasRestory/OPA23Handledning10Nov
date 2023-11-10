9df879cea92d12339b07a37c50bb6cc0455c6463
NiklasRestory <niklas.cullberg@restory.ai>
2023-11-10 13:37:59 +0100

    Add Dragon button implemented

--- src/main/java/org/example/DragonCreatorForm.java
+++ src/main/java/org/example/DragonCreatorForm.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class DragonCreatorForm {
     private JTextField ageField;
@@ -9,10 +12,13 @@ public class DragonCreatorForm {
     private JTextField colorField;
     private JTextField gasLevelField;
     private JButton submitDragonButton;
-    private JList dragonList;
+    private JList dragonJList;
+    private DefaultListModel dragonListModel = new DefaultListModel();
     private JButton removeDragonButton;
 
     private JPanel dragonCreatorPanel;
+
+    ArrayList<Dragon> dragonList = new ArrayList<>();
     private JFrame jFrame = new JFrame("Dragon Creator");
     public DragonCreatorForm() {
         jFrame.setSize(400, 400);
@@ -21,7 +27,27 @@ public class DragonCreatorForm {
         jFrame.setContentPane(dragonCreatorPanel);
         jFrame.setVisible(true);
 
+        dragonJList.setModel(dragonListModel);
 
+        submitDragonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String name = nameField.getText();
+                    int age = Integer.parseInt(ageField.getText());
+                    int wingspan = Integer.parseInt(wingspanField.getText());
+                    String color = colorField.getText();
+                    int gasLevel = Integer.parseInt(gasLevelField.getText());
+
+                    Dragon newDragon = new Dragon(name, age, wingspan, color, gasLevel);
+                    dragonList.add(newDragon);
+                    dragonListModel.addElement(newDragon.getName());
+                }
+                catch (Exception ex) {
+
+                }
+            }
+        });
     }
 
 }
